22e69846fb4c328c6611c3cc7da13c03b89f5405
pksh-23514 <prabhat23514k@gmail.com>
2025-05-14 00:40:26 +0530

    Allocated Device number in the Start function of the Module

---
+++ drivers/pseudo_char_driver/main.h
@@ -0,0 +1,23 @@
+/*
+ * main.h ---> Header file for the Pseudo Device Driver Module
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Macro Definitions */
+
+#define DEVICE_MEM_SIZE 1024
+
+/* Dynamic allocation of Major number */
+#ifndef SCULL_MAJOR
+#define SCULL_MAJOR 0
+#endif
+
+
+
+
+/* Function Prototypes */
+
+
+#endif
